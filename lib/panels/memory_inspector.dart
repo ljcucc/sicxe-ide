f94123e9063e9c36f6cea94a98ebb1b609ba75c3
ljcucc <ljcucc@gmail.com>
2024-03-12 21:19:13 +0800

    remove usage of overview_card from memory inspector

--- lib/panels/memory_inspector.dart
+++ lib/panels/memory_inspector.dart
@@ -16,26 +16,26 @@ class MemoryInspector extends StatelessWidget {
     return Consumer<EmulatorWorkflow>(builder: (context, emulator, _) {
       final mem = emulator.getMemory();
 
-      return OverviewCard(
-        expanded: true,
-        title: Text("Memory overview"),
-        // onInfoOpen: () =>
-        //     Provider.of<DocumentDisplayProvider>(context, listen: false)
-        //         .changeMarkdown("memory.md"),
-        child: Card(
-          elevation: 0,
-          shadowColor: Colors.transparent,
-          child: ListView.builder(
-            padding: EdgeInsets.all(16),
-            addAutomaticKeepAlives: false,
-            itemCount: 0xFFF ~/ 8 + 1,
-            itemBuilder: (context, index) {
-              return Text(
-                "${(index * 8).toRadixString(16).padLeft(5, "0").toUpperCase()}  ${mem.getRange(index * 8, index * 8 + 8).map((e) => e.toRadixString(16).padLeft(2, '0')).join(" ")}",
-                style: GoogleFonts.spaceMono(),
-              );
-            },
-          ),
+      return Card(
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        margin: EdgeInsets.zero,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.all(16),
+                addAutomaticKeepAlives: false,
+                itemCount: 0xFFF ~/ 8 + 1,
+                itemBuilder: (context, index) {
+                  return Text(
+                    "${(index * 8).toRadixString(16).padLeft(5, "0").toUpperCase()}  ${mem.getRange(index * 8, index * 8 + 8).map((e) => e.toRadixString(16).padLeft(2, '0')).join(" ")}",
+                    style: GoogleFonts.spaceMono(),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       );
     });
